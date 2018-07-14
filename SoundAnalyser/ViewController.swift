01c9bc841b1a5e7e018b58922b8d5cea8ff37590
Isaiah Erb <isaiaherb@tdlab.io>
2018-07-13 20:58:18 -0400

    init commit

---
+++ SoundAnalyser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundAnalyser
+//
+//  Created by Isaiah Erb on 2018-07-13.
+//  Copyright © 2018 Isaiah Erb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
